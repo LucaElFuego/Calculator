1f9381902677f53f3cddd9ce0d5ab524df5f304d
LucaElFuego <l.picco.4108@vallauri.edu>
2026-09-16 11:30:17 +0200

    Aggiunti Backgroundcolors

--- Calculator/frm_main.cs
+++ Calculator/frm_main.cs
@@ -12,14 +12,30 @@ namespace Calculator
 {
     public partial class frm_main : Form
     {
-        private char[,] buttons =
+        static private Color OPERATION_BG = Color.LightGray;
+        static private Color NUMBER_BG = Color.WhiteSmoke;
+        static private Color EQUAL_BG = Color.LightSeaGreen;
+
+        public struct Btn_Struct
         {
-            {'%', '\u0152', 'C', '\u232B'},
-            {'\u215F', '\u00B2', '\u221A', '\u00F7'},
-            {'7', '8', '9', 'X'},
-            {'4', '5', '6', '-'},
-            {'1', '2', '3', '+'},
-            {'\u00B1', '0', ',', '='} //\u permette di definire un carattere col codice unicode
+            public char Content;
+            public Color Bg_Color;
+            public Btn_Struct(char content, Color bg_color)
+            {
+                this.Content = content;
+                this.Bg_Color = bg_color;
+            }
+        }
+
+        
+        private Btn_Struct[,] buttons =
+        {
+            {new Btn_Struct('%', OPERATION_BG), new Btn_Struct('\u0152', OPERATION_BG), new Btn_Struct('C', OPERATION_BG), new Btn_Struct('\u232B', OPERATION_BG)},
+            {new Btn_Struct('\u215F', OPERATION_BG), new Btn_Struct('\u00B2', OPERATION_BG), new Btn_Struct('\u221A', OPERATION_BG), new Btn_Struct('\u00F7', OPERATION_BG)},
+            {new Btn_Struct('7', NUMBER_BG), new Btn_Struct('8', NUMBER_BG), new Btn_Struct('9', NUMBER_BG), new Btn_Struct('X', OPERATION_BG)},
+            {new Btn_Struct('4', NUMBER_BG), new Btn_Struct('5', NUMBER_BG), new Btn_Struct('6', NUMBER_BG), new Btn_Struct('-', OPERATION_BG)},
+            {new Btn_Struct('1', NUMBER_BG), new Btn_Struct('2', NUMBER_BG), new Btn_Struct('3', NUMBER_BG), new Btn_Struct('+', OPERATION_BG)},
+            {new Btn_Struct('\u00B1', OPERATION_BG), new Btn_Struct('0', NUMBER_BG), new Btn_Struct(',', OPERATION_BG), new Btn_Struct('=', EQUAL_BG)} //\u permette di definire un carattere col codice unicode
         };
 
         public frm_main()
@@ -47,7 +63,8 @@ namespace Calculator
                     btn.Left = pos_x;
                     btn.Top = pos_y;
                     btn.Font = new Font("Segoe UI", 16);
-                    btn.Text = buttons[i, j].ToString();
+                    btn.Text = buttons[i, j].Content.ToString();
+                    btn.BackColor = buttons[i, j].Bg_Color;
                     this.Controls.Add(btn);
 
                     pos_x += btn_width;
